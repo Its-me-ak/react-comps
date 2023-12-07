1f9b475ebf3f0521c5cc0f7c3148c24c7b7a056a
Mohd Aquib <akibkhan9984@gmail.com>
2023-12-07 23:36:51 +0530

    accordion expendend

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Accordion = ({items}) => {
-    const renderdItems = items.map((item)=>{
+  const [expendedIndex, setExpendedIndex] = useState(0);
+    const renderdItems = items.map((item, index)=>{
+      const isExpended = index === expendedIndex;
+      console.log(isExpended);
         return(
             <div key={item.id}>
                 <div>{item.label}</div>
