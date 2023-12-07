8789ef39a47483ea61d3eb998dcf377f92801f20
Mohd Aquib <akibkhan9984@gmail.com>
2023-12-07 23:50:19 +0530

    accordion state manage

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from 'react'
 
 const Accordion = ({items}) => {
-  const [expendedIndex, setExpendedIndex] = useState(0);
+  const [expendedIndex, setExpendedIndex] = useState(2);
     const renderdItems = items.map((item, index)=>{
       const isExpended = index === expendedIndex;
-      console.log(isExpended);
         return(
             <div key={item.id}>
                 <div>{item.label}</div>
-                <div>{item.content}</div>
+            {isExpended && <div>{item.content}</div>}
             </div>
         )
     })
