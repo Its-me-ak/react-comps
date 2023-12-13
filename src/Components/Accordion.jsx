60a33f460876b06b0d9beeb30a101022aed758d0
Mohd Aquib <akibkhan9984@gmail.com>
2023-12-13 23:48:26 +0530

    accordion css updated

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { GoChevronDown, GoChevronLeft } from "react-icons/go";
 
 const Accordion = ({ items }) => {
   const [expendedIndex, setExpendedIndex] = useState(0);
@@ -9,27 +10,26 @@ const Accordion = ({ items }) => {
   const renderdItems = items.map((item, index) => {
     const isExpended = index === expendedIndex;
 
-    const icon = <span>
-      {isExpended ? 'DOWN' : 'LEFT'}
+    const icon = <span className='text-2xl'> 
+      {isExpended ? <GoChevronDown/> : <GoChevronLeft/>}
     </span>
 
     return (
       <div key={item.id}>
         {/* <div onClick={()=>{setExpendedIndex(index)}}>{item.label}</div> */}
-        <div onClick={() => { handleClick(index) }}>
-          {icon}
+        <div className='flex justify-between items-center cursor-pointer p-3 bg-gray-50 border-b' onClick={() => { handleClick(index) }}>
           {item.label}
-
+          {icon}
         </div>
 
-        {isExpended && <div>{item.content}</div>}
+        {isExpended && <div className='border-b p-5'>{item.content}</div>}
       </div>
     )
   })
   return (
-    <>
+    <div className='border-x border-t rounded'>
       {renderdItems}
-    </>
+    </div>
   )
 }
 
