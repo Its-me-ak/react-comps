2d2d43170f2afe3b259c2a0b1b3743a3cc9b78af
Mohd Aquib <akibkhan9984@gmail.com>
2023-12-13 22:55:33 +0530

    accordion button added

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from 'react'
 
-const Accordion = ({items}) => {
+const Accordion = ({ items }) => {
   const [expendedIndex, setExpendedIndex] = useState(0);
-    const renderdItems = items.map((item, index)=>{
-      const isExpended = index === expendedIndex;
-        return(
-            <div key={item.id}>
-                <div onClick={()=>{setExpendedIndex(index)}}>{item.label}</div>
-            {isExpended && <div>{item.content}</div>}
-            </div>
-        )
-    })
+
+  const handleClick = (nextIndex) => {
+    setExpendedIndex(nextIndex)
+  }
+  const renderdItems = items.map((item, index) => {
+    const isExpended = index === expendedIndex;
+
+    const icon = <span>
+      {isExpended ? 'DOWN': 'LEFT'}
+    </span>
+    console.log(icon);
+
+    return (
+      <div key={item.id}>
+        {/* <div onClick={()=>{setExpendedIndex(index)}}>{item.label}</div> */}
+        <div onClick={() => { handleClick(index) }}>{item.label}</div>
+
+        {isExpended && <div>{item.content}</div>}
+      </div>
+    )
+  })
   return (
     <>
       {renderdItems}
