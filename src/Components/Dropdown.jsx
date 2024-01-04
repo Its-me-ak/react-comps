e2b5334f6a250e02e60845260190e10649b92d6e
Mohd Aquib <akibkhan9984@gmail.com>
2024-01-05 00:02:52 +0530

    dropdown added

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Dropdown = ({ options }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleToggle = () =>{
+        setIsOpen(!isOpen);
+        // setIsOpen((currentIsOpen) => !currentIsOpen);
+    }
+
+    const renderOption = options.map((option)=>{
+        return <div key={option.value}>{option.label}</div>
+    })
   return (
     <>
-    <h1>Dropdown</h1>
+    <div onClick={handleToggle}>Select...</div>
+    {isOpen && <div>{renderOption}</div>}
     </>
   )
 }
