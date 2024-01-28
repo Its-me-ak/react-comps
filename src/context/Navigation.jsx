0c0058ae09d31adbab8c9d5734ce42e694258e82
Mohd Aquib <akibkhan9984@gmail.com>
2024-01-28 17:00:40 +0530

    dropdown style addded

--- src/context/Navigation.jsx
+++ src/context/Navigation.jsx
@@ -1,12 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const NavigationContext = createContext();
 
 const NavigationProvider = ({children}) => {
     const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(()=>{
+        const handler = () =>{
+            setCurrentPath(window.location.pathname)
+        }
+        window.addEventListener("popstate", handler);
+
+        return () =>{
+            window.removeEventListener("popstate", handler);
+        }
+
+    }, [])
+
+    const navigate = (to) =>{
+        window.history.pushState({}, '', to)
+        setCurrentPath(to);
+    }
     return (
-        <NavigationContext.Provider value={{}}>
-            {currentPath}
+        <NavigationContext.Provider value={{currentPath, navigate}}>
             {children}
         </NavigationContext.Provider>
     )
