eef03d0f3d9c14b84e0ae84b5afc52d612ec3c9a
Mohd Aquib <akibkhan9984@gmail.com>
2024-03-30 17:15:10 +0530

    useReducer hook

--- src/Pages/CounterPage.jsx
+++ src/Pages/CounterPage.jsx
@@ -1,39 +1,68 @@
-import { useReducer, useState } from 'react';
+import { useReducer } from 'react';
 import Button from '../Components/Button';
 import Panel from '../Components/Panel';
+import { produce } from "immer"
 
+const INCREMENT_COUNT = "increment"
+const DECREMENT_COUNT = "decrement"
+const SET_VALUE_TO_ADD = "value-to-add"
+const VALUE_ADD_TO_COUNT = "value-add-to-count"
 
-const reducer = (state, action) =>{
-    return {
-      ...state,
-      count: state.count + 1
-    }
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      state.count = state.count + 1
+      return
+
+    case DECREMENT_COUNT:
+      state.count = state.count - 1
+      return;
+
+    case VALUE_ADD_TO_COUNT:
+      state.count = state.count + state.valueToAdd
+      state.valueToAdd = 0
+      return;
+
+    case SET_VALUE_TO_ADD:
+      state.valueToAdd = action.payload
+      return;
+      
+    default:
+      return state
+  }
 }
 const CounterPage = ({ intialCount }) => {
-  const [state, dispatch] = useReducer(reducer, {
-     count: intialCount ,
-      valueToAdd: 0
-    });
-  // const [count, setCount] = useState(intialCount);
-  // const [valueToAdd, setValueToAdd] = useState(0)
+  const [state, dispatch] = useReducer(produce(reducer), {
+    count: intialCount,
+    valueToAdd: 0
+  });
+  console.log(state);
 
   const increment = () => {
-    dispatch()
+    dispatch({
+      type: INCREMENT_COUNT
+    })
   }
 
   const decrement = () => {
-    // setCount(count - 1)
+    dispatch({
+      type: DECREMENT_COUNT
+    })
   }
 
   const handleChamge = (event) => {
     const value = parseInt(event.target.value) || 0;
-    // setValueToAdd(value)
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value
+    })
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // setCount(count+valueToAdd)
-    // setValueToAdd(0)
+    dispatch({
+      type: VALUE_ADD_TO_COUNT
+    })
   }
   return (
     <>
